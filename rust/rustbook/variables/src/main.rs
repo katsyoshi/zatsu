823c510637b031b5950277028b9ecca1dbc43458
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-12-31 21:14:07 +0900

    variables

---
+++ rust/rustbook/variables/src/main.rs
@@ -0,0 +1,16 @@
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("The value of space is: {}", spaces);
+
+    let spaces = "   ";
+    spaces = spaces.len();
+}
