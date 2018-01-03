850da0b17a40fb0bd8a4d13ed1116e2744d09b57
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-03 16:55:40 +0900

    4.2 references and borrowing

---
+++ rust/rustbook/references_and_borrowing/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+    let mut s = String::from("hello");
+
+    // let r1 = &s;
+    // let r2 = &s;
+    // let r3 = &mut s;
+    change(&mut s);
+    let reference_to_nothing = no_dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s
+// }
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
+}
