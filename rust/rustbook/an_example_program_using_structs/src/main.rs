11fb0dd6e75b2ae152b7e5a6c7fb33249cfdce02
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-06 06:59:12 +0900

    refactoring with tuples

--- rust/rustbook/an_example_program_using_structs/src/main.rs
+++ rust/rustbook/an_example_program_using_structs/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let dimension = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(dimension)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
