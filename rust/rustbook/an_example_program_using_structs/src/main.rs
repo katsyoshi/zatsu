d2d76b5f9c2182cbb2636caa89286f9730039b09
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-06 16:17:41 +0900

    Assosiated Functions

--- rust/rustbook/an_example_program_using_structs/src/main.rs
+++ rust/rustbook/an_example_program_using_structs/src/main.rs
@@ -12,13 +12,14 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    let sq = Rectangle::square(3);
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("sq is {:?}", sq );
 }
