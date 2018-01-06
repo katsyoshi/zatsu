038306043a7f8187c3382fa674b575d91da8a998
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-06 16:15:03 +0900

    method and more parameters

--- rust/rustbook/an_example_program_using_structs/src/main.rs
+++ rust/rustbook/an_example_program_using_structs/src/main.rs
@@ -8,15 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
-    let rect = Rectangle { width: 30, height: 50 };
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect.area()
-    );
-
-    println!("rect is {:?}", rect);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
