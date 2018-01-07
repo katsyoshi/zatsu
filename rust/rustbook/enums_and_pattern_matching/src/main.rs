f5cad1b04e0b8e2bec6c4d7c6d79cd873e1e35ee
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-07 10:49:05 +0900

    refactoring enum IpAddr

--- rust/rustbook/enums_and_pattern_matching/src/main.rs
+++ rust/rustbook/enums_and_pattern_matching/src/main.rs
@@ -1,21 +1,9 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 }
