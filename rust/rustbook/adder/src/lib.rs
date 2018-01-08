2ecfa31995df9442f7b906512274f61fb8b41c53
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-08 16:29:13 +0900

    Checking for Panics with `should_panic`

--- rust/rustbook/adder/src/lib.rs
+++ rust/rustbook/adder/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn greeting(name: &str) -> String {
-    format!("Hello")
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,11 +21,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`", result
-        );
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
