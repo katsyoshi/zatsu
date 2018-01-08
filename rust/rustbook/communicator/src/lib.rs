085888aae60425101d3c9e7818b6cf79ef32b436
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-08 09:13:52 +0900

    Usign `super` to Access a Parent Module

--- rust/rustbook/communicator/src/lib.rs
+++ rust/rustbook/communicator/src/lib.rs
@@ -1,10 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
 pub mod client;
 pub mod network;
+
+#[cfg(test)]
+mod tests {
+    use super::client;
+
+    #[test]
+    fn it_works() {
+        client::connect();
+    }
+}
