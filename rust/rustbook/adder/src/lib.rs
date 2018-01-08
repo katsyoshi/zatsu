69cff5b93e8d79ca7490d61c774a93d5ad04276f
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-08 16:19:25 +0900

    Testing Equality with the `assert_eq!` and `assert_ne!` Macros

--- rust/rustbook/adder/src/lib.rs
+++ rust/rustbook/adder/src/lib.rs
@@ -1,13 +1,5 @@
-#[derive(Debug)]
-pub struct Rectangle {
-    length: u32,
-    width: u32,
-}
-
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        self.length > other.length && self.width > other.width
-    }
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[cfg(test)]
@@ -15,18 +7,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle { length: 8, width: 7 };
-        let smaller = Rectangle { length: 5, width: 1 };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_can_hold_smaller() {
-        let larger = Rectangle { length: 8, width: 7 };
-        let smaller = Rectangle { length: 5, width: 1 };
-
-        assert!(!smaller.can_hold(&larger));
+    fn it_adds_two(){
+        assert_eq!(4, add_two(2));
     }
 }
