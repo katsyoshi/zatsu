bcd51632d75c72a6bab06403c2fd76dffd01578c
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-08 16:15:57 +0900

    Checking Results with the `assert!` Macro

--- rust/rustbook/adder/src/lib.rs
+++ rust/rustbook/adder/src/lib.rs
@@ -1,12 +1,32 @@
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(larger.can_hold(&smaller));
     }
 
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn smaller_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(!smaller.can_hold(&larger));
     }
 }
