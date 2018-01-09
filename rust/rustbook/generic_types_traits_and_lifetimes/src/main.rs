2bde5d72949c65d43bd31f4b0ff3abdcacada51b
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-09 19:55:42 +0900

    Using Generic Data Types in Struct Definitions

--- rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
+++ rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
@@ -1,23 +1,9 @@
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest number is {}", result);
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }
