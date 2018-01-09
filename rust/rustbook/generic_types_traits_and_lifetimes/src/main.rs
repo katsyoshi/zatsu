be23fec26d18a776b29bb988c48f60d53241caf3
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-09 20:07:18 +0900

    impl fn mixup

--- rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
+++ rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
@@ -1,15 +1,21 @@
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
-    println!("p.x = {}", p.x());
+    let p1 = Point { x: 5, y: 10 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
