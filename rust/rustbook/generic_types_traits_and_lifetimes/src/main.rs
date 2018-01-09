946cfd38c22bb51962ce084ea2c66ef3c06a2d40
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-09 19:51:33 +0900

    Using Generic Data Types in Function Definitions

--- rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
+++ rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
@@ -1,16 +1,4 @@
-fn largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -25,13 +13,11 @@ fn largest_char(list: &[char]) -> char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
-   assert_eq!(result, 100);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&number_list);
+    let result = largest(&char_list);
     println!("The largest number is {}", result);
-   assert_eq!(result, 6000);
 }
