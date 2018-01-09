dd367e537591b6661e11e988d10bf0d2745eba8c
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-09 19:48:39 +0900

    10.1 Generic Data Types

--- rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
+++ rust/rustbook/generic_types_traits_and_lifetimes/src/main.rs
@@ -1,4 +1,16 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -13,13 +25,13 @@ fn largest(list: &[i32]) -> i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
    assert_eq!(result, 100);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest(&number_list);
+    let result = largest_char(&number_list);
     println!("The largest number is {}", result);
    assert_eq!(result, 6000);
 }
