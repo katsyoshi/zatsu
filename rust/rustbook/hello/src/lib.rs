936eedff947d9f93f51cb7a6151b294049ee53a4
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 23:02:38 +0900

    Storing Threads in the Pool

--- rust/rustbook/hello/src/lib.rs
+++ rust/rustbook/hello/src/lib.rs
@@ -1,15 +1,44 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
-    pub fn new(size: u32) -> Result<ThreadPool, PoolCreationError> {
+    pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool {
+            workers
+        }
     }
 
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
     {
+    }
+}
+
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread,
+        }
     }
 }
