f09fe071c795509f93effdc48ec369ed08dc9e8f
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 21:06:48 +0900

    Storing Matching Lines

--- rust/rustbook/minigrep/src/lib.rs
+++ rust/rustbook/minigrep/src/lib.rs
@@ -47,9 +47,13 @@ Pick three.";
 }
 
 pub fn search <'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
     for line in contents.lines() {
         if line.contains(query) {
-
+            results.push(line);
         }
     }
+
+    results
 }
