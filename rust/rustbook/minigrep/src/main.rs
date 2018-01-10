f4740c77c20ad1ef34b51d925443fcba781e31ee
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 20:31:47 +0900

    Improving the Error Message

--- rust/rustbook/minigrep/src/main.rs
+++ rust/rustbook/minigrep/src/main.rs
@@ -23,15 +23,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
