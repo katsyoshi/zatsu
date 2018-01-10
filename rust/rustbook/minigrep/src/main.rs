1b5fdc2ef29450e9104b4871c0208adc216a46da
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 20:42:05 +0900

    Returning Errors from the `run` Function

--- rust/rustbook/minigrep/src/main.rs
+++ rust/rustbook/minigrep/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,11 +18,13 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
-    let mut f = File::open(config.filename).expect("file not found");
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("somthing went wrong reading the file");
+    f.read_to_string(&mut contents)?;
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
