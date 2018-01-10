3b1ac939f747f370945364cef36b54fa570af42b
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 21:58:19 +0900

    Returning Real HTML

--- rust/rustbook/hello/src/main.rs
+++ rust/rustbook/hello/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -14,10 +15,14 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
