190ed3df6f2ef1e05c2ac857b29136b2b3a2bb00
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 22:18:10 +0900

    Creating a Similar Interface for ThreadPool

--- rust/rustbook/hello/src/main.rs
+++ rust/rustbook/hello/src/main.rs
@@ -7,11 +7,12 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool::execute(|| {
             handle_connection(stream);
         });
     }
