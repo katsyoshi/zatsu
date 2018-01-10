2d07025dcadd0c5c89fff6e76bbc7bcc75290823
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-10 20:27:06 +0900

    Grouping Configuration Values

--- rust/rustbook/minigrep/src/main.rs
+++ rust/rustbook/minigrep/src/main.rs
@@ -5,12 +5,12 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
@@ -18,9 +18,14 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
