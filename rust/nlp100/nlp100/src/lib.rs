6c363df04bff79801bcc35e5b1abc789a18cf942
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-18 01:09:59 +0900

    impl NLP100 new methods

--- rust/nlp100/nlp100/src/lib.rs
+++ rust/nlp100/nlp100/src/lib.rs
@@ -1,12 +1,29 @@
-struct NLP100 {
+pub struct NLP100 {
+    origin: String,
     words: Vec<String>,
     chars: Vec<String>,
 }
 
+impl NLP100 {
+    fn new(script: &str) -> NLP100 {
+        let chars = script.chars().map(|m| m.to_string()).collect::<Vec<String>>();
+        let words = script.split(' ').map(|m| m.to_string()).collect::<Vec<String>>();
+        NLP100 {
+            words,
+            chars,
+            origin: script.to_string(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use NLP100;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let nlp100 = NLP100::new("hello");
+        assert_eq!(nlp100.origin, "hello");
+        assert_eq!(nlp100.words, ["hello"].to_vec());
+        assert_eq!(nlp100.chars, ["h", "e", "l", "l", "o"].to_vec());
     }
 }
