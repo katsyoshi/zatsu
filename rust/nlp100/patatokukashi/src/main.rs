0715d6e80f11c7e1574aeaa0ba36ab76c0a33599
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-18 01:22:32 +0900

    use crate nlp100

--- rust/nlp100/patatokukashi/src/main.rs
+++ rust/nlp100/patatokukashi/src/main.rs
@@ -1,6 +1,9 @@
+extern crate nlp100;
+use nlp100::NLP100;
+
 fn main() {
-    let chars = "パタトクカシーー".chars().collect::<Vec<char>>();
+    let nlp100 = NLP100::new("パタトクカシーー");
     let mut s = String::new();
-    for (_i, c) in chars.iter().enumerate().filter(|&(i, _c)| i % 2 == 0 ) { s.push(*c); }
+    for (_i, c) in nlp100.chars.iter().enumerate().filter(|&(i, _c)| i % 2 == 0 ) { s += c; }
     println!("{}", s);
 }
