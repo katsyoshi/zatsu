df69143bcde55576f1a7dcf293383ac7294f589f
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-17 23:36:46 +0900

    wrong read question

--- rust/nlp100/pi/src/main.rs
+++ rust/nlp100/pi/src/main.rs
@@ -1,3 +1,15 @@
+fn count(word: &str) -> usize {
+    word.chars().count()
+}
+
+fn count_list(words: Vec<&str>) -> Vec<usize> {
+    let mut v: Vec<usize> = Vec::new();
+    for w in words {
+        v.push(count(w));
+    }
+    v
+}
+
 fn sort(words: &mut Vec<&str>) {
     words.sort_by(|w, n| (w.to_lowercase().chars().collect::<Vec<char>>()[0] as u8).cmp(&(n.to_lowercase().chars().collect::<Vec<char>>()[0] as u8)));
     println!("{:?}", words);
@@ -5,5 +17,5 @@ fn sort(words: &mut Vec<&str>) {
 
 fn main() {
     let mut s: Vec<&str> = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".split(' ').collect();
-    sort(&mut s);
+    println!("{:?}", count_list(s));
 }
