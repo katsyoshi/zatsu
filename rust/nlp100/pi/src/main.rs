ff7d2dfe8c8b44bd9fc7da65618635241f0569b4
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-18 22:37:41 +0900

    use this repo crate

--- rust/nlp100/pi/src/main.rs
+++ rust/nlp100/pi/src/main.rs
@@ -1,16 +1,10 @@
+extern crate nlp100;
+extern crate regex;
+use regex::Regex;
+use nlp100::NLP100;
+
 fn count(word: &str) -> u8 {
-    let chars: Vec<char> = word.chars().collect();
-    let mut count = 0;
-    for c in chars {
-        let var: u8 = c as u8;
-        match var {
-            48 ... 58 => { count = count + 1; },
-            65 ... 91 => { count = count + 1; },
-            96 ... 123 => { count = count + 1; },
-            _ => (),
-        }
-    }
-    count
+    Regex::new(r"\W+").unwrap().split(word).filter(|f| *f.to_string() != String::from("")).collect().len()
 }
 
 fn count_list(words: Vec<&str>) -> Vec<u8> {
