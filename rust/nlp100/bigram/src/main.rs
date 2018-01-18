4f741f93ca48dc752dbf13b99ba30a426093e8e3
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-19 01:13:28 +0900

    move bigram, to NLP100

--- rust/nlp100/bigram/src/main.rs
+++ rust/nlp100/bigram/src/main.rs
@@ -1,36 +1,16 @@
 extern crate nlp100;
 use nlp100::NLP100;
 
-fn bigram(words: Vec<String>) -> Vec<Vec<String>>{
-    let mut result: Vec<Vec<String>> = Vec::new();
-    let mut itr = words.iter();
-    let mut now = itr.next().unwrap().to_string();
-
-    loop {
-        let mut ngram: Vec<String> = Vec::new();
-        ngram.push(now);
-        match itr.next() {
-            Some(s) => {
-                now = s.to_string();
-                ngram.push(s.to_string());
-                result.push(ngram);
-            },
-            None => break,
-        }
-    }
-
-    result
-}
-
 fn main() {
     let phrase = NLP100::new("I am an NLPer");
     println!("\n===word bi-gram");
-    for word in bigram(phrase.words) {
-        println!("{:?}", word);
+    for word in phrase.ngram(2, true) {
+        println!("{}", word);
     }
 
+    let phrase = NLP100::new("I am an NLPer");
     println!("\n===charactor bi-gram");
-    for word in bigram(phrase.chars) {
-        println!("{:?}", word);
+    for word in phrase.ngram(2, false) {
+        println!("\"{}\"", word);
     }
 }
