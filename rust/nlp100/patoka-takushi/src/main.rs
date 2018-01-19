f4a4de7348ead2640cd41aea30da81136916f554
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-19 20:26:56 +0900

    use zip

--- rust/nlp100/patoka-takushi/src/main.rs
+++ rust/nlp100/patoka-takushi/src/main.rs
@@ -1,24 +1,16 @@
-use std::env;
+fn concat(t: (Vec<char>, Vec<char>)) {
+    let (f, s) = t;
+    let mut r = String::new();
 
-fn concat(mut args: env::Args) {
-    args.next();
-    let a = args.next().unwrap().chars().collect::<Vec<char>>();
-    let b = args.next().unwrap().chars().collect::<Vec<char>>();
-    let l = if a.len() > b.len() {
-        a.len()
-    } else {
-        b.len()
-    };
-
-    let mut word = String::new();
-
-    for x in 0..l {
-        word.push(a[x]);
-        word.push(b[x]);
+    for (x, y) in f.iter().zip(s.iter()) {
+        r.push_str(&format!("{}{}", x, y));
     }
-    println!("{}", word);
+    println!("{}", r);
 }
 
 fn main() {
-    concat(env::args());
+    let p: Vec<char> = String::from("パトカー").chars().collect();
+    let t: Vec<char> = String::from("タクシー").chars().collect();
+    let f = (p, t);
+    concat(f);
 }
