6c56106b0353e5b6d1a5b5202ff37ed0248afabe
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-19 19:36:27 +0900

    remove count_list

--- rust/nlp100/pi/src/main.rs
+++ rust/nlp100/pi/src/main.rs
@@ -1,15 +1,6 @@
 extern crate nlp100;
 extern crate regex;
-use regex::Regex;
 use nlp100::NLP100;
-
-fn count(word: &str) -> u8 {
-    Regex::new(r"\W+").unwrap().split(word).filter(|f| *f.to_string() != String::from("")).collect().len()
-}
-
-fn count_list(words: Vec<&str>) -> Vec<u8> {
-    words.iter().map(|w| count(w)).collect::<Vec<u8>>()
-}
 
 fn sort(words: &mut Vec<&str>) {
     words.sort_by(|w, n| (w.to_lowercase().chars().collect::<Vec<char>>()[0] as u8).cmp(&(n.to_lowercase().chars().collect::<Vec<char>>()[0] as u8)));
@@ -17,6 +8,6 @@ fn sort(words: &mut Vec<&str>) {
 }
 
 fn main() {
-    let s: Vec<&str> = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".split(' ').collect();
-    println!("{:?}", count_list(s));
+    let nlp100 = NLP100::new("Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.");
+    println!("{:?}", nlp100.char_count_list());
 }
