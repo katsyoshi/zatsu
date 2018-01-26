70ae42d2e224293836a677bd5daa9d5bda850b44
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-01-26 20:09:22 +0900

    skip lines

--- rust/nlp100/tail/src/main.rs
+++ rust/nlp100/tail/src/main.rs
@@ -7,12 +7,13 @@ fn main() {
     let args = env::args().skip(1).collect::<Vec<String>>();
     let file = File::open(&args[0]).unwrap();
     let takes = (&args[1]).to_string().parse::<usize>().unwrap();
+    let br = BufReader::new(&file).lines();
+    let skips = br.count() - takes;
+    let file = File::open(&args[0]).unwrap();
+    let lines = BufReader::new(&file).lines().skip(skips);
 
-    let br = BufReader::new(file).lines().map(|m| m.unwrap().to_string()).collect::<Vec<String>>();
-    let rev = br.iter().rev().take(takes).map(|m| m.to_string()).collect::<Vec<String>>();
-
-    for line in rev.iter().rev() {
-        println!("{}", line);
+    for line in lines {
+        println!("{}", line.unwrap());
     }
 }
 
